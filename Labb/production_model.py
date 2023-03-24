972060e47338ea60986748fd7b641404fbed5dcc
William L <41084946+helveticae@users.noreply.github.com>
2023-03-24 15:32:58 +0100

    cleanup

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -1,9 +1,4 @@
-#Skapa ett nytt skript: **production_model.py**, ladda in **test_samples.csv** och din modell. Använd
-#```joblib.load()``` för att ladda in en .pkl-fil. Gör prediction på de 100 datapunkterna och exportera en fil "prediction.csv" som ska innehålla kolumnerna med ifyllda värden:
-#
-#- probability class 0
-#- probability class 1
-#- prediction
+
 
 import pandas as pd
 import joblib
